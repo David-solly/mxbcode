b62d2596b1acc8d9c90e806ea1d759251f8e4c04
David-solly <David.solomon_@outlook.com>
2020-08-27 05:58:55 +0100

    flagged prod endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -40,9 +40,9 @@ var (
 	shutdown  = make(chan bool)
 	// API registration endpoint
 	//
-	// url      string = "http://127.0.0.1:8080/sensor-onboarding-sample"
+	url      string = "http://127.0.0.1:8080/sensor-onboarding-sample"
 	urlDebug string = "http://127.0.0.1:8080/"
-	url      string = "http://europe-west1-machinemax-dev-d524.cloudfunctions.net/sensor-onboarding-sample"
+	// url      string = "http://europe-west1-machinemax-dev-d524.cloudfunctions.net/sensor-onboarding-sample"
 )
 
 // parse variable from commandline or exec command
